94115dc218f0c78df797c5310c9e13e211924704
Jérémy VIGNELLES <jeremy.vignelles@dev3i.fr>
2021-10-29 11:29:54 +0200

    Initial commit

---
+++ ChildProcess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChildProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ RootProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace RootProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var childProcess = Process.Start(new ProcessStartInfo
+            {
+                FileName = "ChildProcess.exe",
+                UseShellExecute = false,
+                RedirectStandardOutput = true
+            });
+
+            Console.Out.Write(childProcess.StandardOutput.ReadToEnd());
+        }
+    }
+}
